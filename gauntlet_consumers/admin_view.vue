85a2f09a925fea7b11763b5d68efc8dfa1e847bb
Wayne <songyanyu607@outlook.com>
2026-07-04 21:12:52 +0800

    test: add cross-pr python frontend gauntlet

---
+++ gauntlet_consumers/admin_view.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <img :src="profile.avatar">
+    <div v-html="profile.bio"></div>
+    <button @click="go(profile.next)">Continue</button>
+  </main>
+</template>
+
+<script lang="ts">
+import { RawProfileCard, SafeLabel } from "gauntlet_fullstack/seed_frontend";
+
+export default {
+  props: {
+    profile: { type: Object, required: true },
+  },
+  methods: {
+    go(next: string) {
+      window.location.href = next;
+    },
+    renderBridge(html: string) {
+      return RawProfileCard({ html });
+    },
+    safeText(text: string) {
+      return SafeLabel({ text });
+    },
+  },
+};
+</script>

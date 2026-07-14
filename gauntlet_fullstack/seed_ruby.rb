5b652c663656e2ecab46f430f1b57e89e0462289
song yanyu <166493970+Wayne0607@users.noreply.github.com>
2026-07-14 10:35:54 +0800

    test(eval): add gauntlet_fullstack/seed_ruby.rb

---
+++ gauntlet_fullstack/seed_ruby.rb
@@ -0,0 +1,24 @@
+require "yaml"
+require "open3"
+
+module JobRuntime
+  def self.normalize_slug(slug)
+    slug.to_s.gsub(/[^a-z0-9_-]/, "")
+  end
+
+  def self.load_yaml(blob)
+    YAML.load(blob)
+  end
+
+  def self.run_shell(cmd)
+    system(cmd)
+  end
+
+  def self.dynamic_call(name, payload)
+    send(name, payload)
+  end
+
+  def self.capture(command)
+    Open3.capture3(command)
+  end
+end

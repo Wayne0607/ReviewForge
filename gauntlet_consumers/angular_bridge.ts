55635a0bf3816990703012ba5f490cde670f8a5b
Wayne <songyanyu607@outlook.com>
2026-07-15 16:03:53 +0800

    test: v6 cross-PR downstream consumer fixture

---
+++ gauntlet_consumers/angular_bridge.ts
@@ -0,0 +1,15 @@
+import { AdminComponent } from "gauntlet_fullstack/admin.component";
+
+export class AngularBridge {
+  constructor(private admin: AdminComponent) {}
+
+  showMessage(rawHtml: string) {
+    return this.admin.trustOperatorHtml(rawHtml);
+  }
+
+  renderTemplate(rawHtml: string) {
+    const holder = document.createElement("div");
+    holder.innerHTML = rawHtml;
+    return holder;
+  }
+}

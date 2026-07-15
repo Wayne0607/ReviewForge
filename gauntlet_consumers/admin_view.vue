904188f2e9aa582aed603447f6c8c8e398b465d7
Wayne <songyanyu607@outlook.com>
2026-07-15 17:48:07 +0800

    test: add v7 cross-PR consumer benchmark

---
+++ gauntlet_consumers/admin_view.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <img :src="profile.avatar">
+    <div v-html="profile.bio"></div>
+    <button @click="go(profile.next)">Continue</button>
+  </main>
+</template>
+
+<script lang="ts">
+import { AccountLabel, RawProfileCard } from "gauntlet_fullstack/seed_frontend";
+
+export default {
+  props: {
+    profile: { type: Object, required: true },
+  },
+  methods: {
+    go(next: string) {
+      window.location.href = next;
+    },
+    renderBridge(html: string) {
+      return RawProfileCard({ html });
+    },
+    labelText(text: string) {
+      return AccountLabel({ text });
+    },
+  },
+};
+</script>
